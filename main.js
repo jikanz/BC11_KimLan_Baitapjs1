c985d804a4d8c11ccbc966f63447c47a6239c35d
jikanz <kl971995@gmail.com>
2021-05-30 19:03:58 +0700

    sua bai

--- main.js
+++ main.js
@@ -4,11 +4,12 @@ document.getElementById("btnTinhTien").onclick = function (event) {
     var matHang = document.getElementById("select").value;
     var soLuong = document.getElementById("txtSoLuong1").value;
     var gia = document.getElementById("txtGia").value;
-    if (100>soLuong >=50) {
-        var tongTien = (matHang*49*gia)+((matHang*(soLuong-49)*gia)*92/100);
-    }else if (soLuong>100){
+    if (soLuong >100) {
         var tongTien = (matHang*49*gia)+((matHang*51*gia)*92/100)+((matHang*(soLuong-100)*gia)*88/100);
-    } else if(soLuong<50){
+    }else if (soLuong>=50){
+        var tongTien = (matHang*49*gia)+((matHang*(soLuong-49)*gia)*92/100);
+        
+    } else {
         var tongTien = (matHang*soLuong*gia);
     }
     var info = "<div class='alert alert-success'>"+ tongTien +"</div>";
